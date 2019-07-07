9a1c9651bb38d58daea5fcc9619a39f9ccda41b5
哎哟不错哦 <43077260@qq.com>
2019-07-07 09:55:17 +0800

    安装element-ui

---
+++ src/views/first.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="article">
+    <p>[重磅] 今天油价喜降5毛钱，各地加油站排成长龙</p>
+    <p>[重磅] 今天油价喜降5毛钱，各地加油站排成长龙</p>
+    <p>[重磅] 今天油价喜降5毛钱，各地加油站排成长龙</p>
+    <p>[重磅] 今天油价喜降5毛钱，各地加油站排成长龙</p>
+    <p>[重磅] 今天油价喜降5毛钱，各地加油站排成长龙</p>
+    <p>[重磅] 今天油价喜降5毛钱，各地加油站排成长龙</p>
+  </div>
+</template>
+
+<style scoped>
+.article {
+  margin: 40px 0;
+}
+.article p {
+  text-decoration: underline;
+  line-height: 40px;
+  font-size: 18px;
+}
+</style>
